400941e95f495c15e1dbcc87fc1427b371ac8155
Showmik Roy <showmik.roy@gmail.com>
2024-09-23 14:06:55 -0700

    made terminal conditionally use powershell if on windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Optionally setup the terminal to use
+-- This sets `vim.o.shell` and does some additional configuration for:
+-- * pwsh
+-- * powershell
+LazyVim.terminal.setup("pwsh")
+
 -- Keeps text cursor 16 lines away from the top or bottom of the screen at all times
 vim.opt.scrolloff = 16
