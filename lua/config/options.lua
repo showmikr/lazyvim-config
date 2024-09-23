f55634a43b43c6002e32a664a7c4d082bfb79b0c
Showmik Roy <showmik.roy@gmail.com>
2024-09-23 14:06:55 -0700

    added scrolloff option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Keeps text cursor 16 lines away from the top or bottom of the screen at all times
+vim.opt.scrolloff = 16
