9fbf6a21134ad0db6ccb9183be9f87c88fa60fd5
Showmik Roy <showmik.roy@gmail.com>
2024-09-29 15:25:16 -0700

    Back out "configured neovim to write files with unix style newlines"
    This backs out commit bc86ba931ece8fb762b98521f0b10e4c362aa8bf.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,10 +12,6 @@ end
 
 maybe_use_powershell()
 
--- force all new files created in vim to have unix style newlines "\n"
-vim.opt.fileformat = "unix"
-vim.opt.fileformats = "unix,dos" -- try to read files with unix style newlines first then windows style
-
 -- Keeps text cursor 16 lines away from the top or bottom of the screen at all times
 vim.opt.scrolloff = 16
 
