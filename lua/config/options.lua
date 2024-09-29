bc86ba931ece8fb762b98521f0b10e4c362aa8bf
Showmik Roy <showmik.roy@gmail.com>
2024-09-28 20:17:41 -0700

    configured neovim to write files with unix style newlines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,10 @@
 -- * powershell
 LazyVim.terminal.setup("pwsh")
 
+-- force all new files created in vim to have unix style newlines "\n"
+vim.opt.fileformat = "unix"
+vim.opt.fileformats = "unix,dos" -- try to read files with unix style newlines first then windows style
+
 -- Keeps text cursor 16 lines away from the top or bottom of the screen at all times
 vim.opt.scrolloff = 16
 
