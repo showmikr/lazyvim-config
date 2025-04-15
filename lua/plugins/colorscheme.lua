364d92b3ce8553093fa650da2bc3ffcfc5a9915a
Showmik Roy <showmik.roy@gmail.com>
2025-04-15 15:15:48 -0700

    changed colorscheme to onedark pro

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,24 @@
 return {
+  -- -- sonokai color scheme commented out until I plan to use it again
+  -- {
+  --   "sainnhe/sonokai",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.sonokai_style = "shusia"
+  --     vim.g.sonokai_enable_italic = true
+  --     vim.g.sonokai_better_performance = 1
+  --   end,
+  -- },
   {
-    "sainnhe/sonokai",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_style = "shusia"
-      vim.g.sonokai_enable_italic = true
-      vim.g.sonokai_better_performance = 1
-    end,
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "sonokai",
+      -- colorscheme = "sonokai",
+      colorscheme = "onedark",
     },
   },
 }
